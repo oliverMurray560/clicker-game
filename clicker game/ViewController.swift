73b202da5b4ce2603ff82c17f92010960e2133a3
Oliver Murray <oliver.murray24@d155.org>
2023-10-17 09:16:02 -0500

    finushed 2

--- clicker game/ViewController.swift
+++ clicker game/ViewController.swift
@@ -5,15 +5,60 @@
 //  Created by OLIVER MURRAY on 10/4/23.
 //
 
+
+protocol ViewControllerDelegate{
+    
+    func upgrade(percent: Double)
+    func minus(amount: Double)
+    func show() -> Double
+}
+
+
+
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ViewControllerDelegate {
+    
+    
+    func show() -> Double {
+        return points
+    }
+    
+    func minus(amount: Double) {
+        points = points - amount
+    }
+    
+    func upgrade(percent: Double) {
+     factor = percent
+    }
+    
 
+    @IBOutlet weak var pointsLabel: UILabel!
+    var factor : Double = 1
+    var points : Double = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        pointsLabel.text = String(points)
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        pointsLabel.text = String(points)
     }
 
-
+    
+    @IBAction func clickAction(_ sender: Any) {
+        points += 1 * factor
+        pointsLabel.text = String(points)
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nvc = segue.destination as! ViewControllerShop
+        nvc.delegate = self
+    }
+    
+    
+    
+    
 }
 
--- clicker game/ViewControllerShop.swift
+++ clicker game/ViewControllerShop.swift
@@ -9,21 +9,60 @@ import UIKit
 
 class ViewControllerShop: UIViewController {
 
+    var points : Double = 0
+    var delegate : ViewControllerDelegate!
+    var per : Double = 1.0
+    @IBOutlet weak var pointsLabel2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        points = delegate.show()
+        pointsLabel2.text = String(points)
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func firstShop(_ sender: UIButton) {
+        
+        if  points >= 10{
+            points = points - 10
+            per = per + 1
+            delegate.upgrade(percent: per)
+            delegate.minus(amount: 10)
+            pointsLabel2.text = String(points)
+        }
     }
-    */
-
+    
+    @IBAction func shop2(_ sender: Any) {
+        
+        if points >= 120{
+          points = points - 120
+            per = per + 10
+            delegate.upgrade(percent: per)
+            delegate.minus(amount: 120)
+            pointsLabel2.text = String(points)
+            
+        }
+        
+    }
+    
+    @IBAction func shop3(_ sender: Any) {
+        
+        if points >= 1500{
+           points = points - 1500
+            per = per + 100
+            delegate.upgrade(percent: per)
+            delegate.minus(amount: 1500)
+            pointsLabel2.text = String(points)
+            
+            
+            
+        }
+        
+    }
+    
+    
+    
+    
+    
 }
